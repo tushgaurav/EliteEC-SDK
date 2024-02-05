dd267e3b9b3b1ab0ad67bb50bd88f92c8e22c73d
tushgaurav <tushar.gaurav416@gmail.com>
2024-02-05 12:42:08 +0530

    version bump

--- src/robot/__init__.py
+++ src/robot/__init__.py
@@ -4,7 +4,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 _cfg = tomllib.loads(resources.read_text("robot", "config.toml"))
 DOWNLOADS = _cfg["website"]["download_url"]
