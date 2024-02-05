a592481c3bc7c79714f647b3692db760fad34b0d
tushgaurav <tushar.gaurav416@gmail.com>
2024-02-05 12:20:27 +0530

    fixed execute function bug

--- src/robot/SDK.py
+++ src/robot/SDK.py
@@ -9,7 +9,7 @@ class Robot:
         self.connect_success = False
         self.sock = None
 
-    def sendCMD(self,cmd,params=None,id=1):
+    def sendCMD(self, cmd, params=None, id=1):
         '''
         Note: This function should not be used directly, instead use the execute function.
 
@@ -25,24 +25,25 @@ class Robot:
         '''
         sock = self.sock
 
-        if(not params):
-            params=[]
+        if (not params):
+            params = []
         else:
-            params=json.dumps(params)
-        sendStr="{{\"method\":\"{0}\",\"params\":{1} ,\"jsonrpc\":\"2.0\",\"id\":{2}}}".format(cmd,params,id)+"\n"
-        
+            params = json.dumps(params)
+        sendStr = "{{\"method\":\"{0}\",\"params\":{1} ,\"jsonrpc\":\"2.0\",\"id\":{2}}}".format(
+            cmd, params, id)+"\n"
+
         try:
-            sock.sendall(bytes(sendStr,"utf-8"))
-            ret=sock.recv(1024)
-            jdata=json.loads(str(ret,"utf-8"))
-            if("result" in jdata.keys()):
-                return (True,json.loads(jdata["result"]),jdata["id"])
-            elif("error" in jdata.keys()):
-                return (False,jdata["error"],jdata["id"])
+            sock.sendall(bytes(sendStr, "utf-8"))
+            ret = sock.recv(1024)
+            jdata = json.loads(str(ret, "utf-8"))
+            if ("result" in jdata.keys()):
+                return (True, json.loads(jdata["result"]), jdata["id"])
+            elif ("error" in jdata.keys()):
+                return (False, jdata["error"], jdata["id"])
             else:
-                return (False,None,None)
+                return (False, None, None)
         except Exception as e:
-            return (False,None,None)
+            return (False, None, None)
 
     def connect(self):
         '''
@@ -51,15 +52,15 @@ class Robot:
         Returns:
             list: [bool, str] -> [True, "Robot connected at IP (ACTUAL IP)"] or [False, "Cannot connect at IP (ACTUAL IP)"]
         '''
-        def connectETController(ip,port=8055):
+        def connectETController(ip, port=8055):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
-                sock.connect((ip,port))
-                return (True,sock)
+                sock.connect((ip, port))
+                return (True, sock)
             except Exception as e:
                 sock.close()
                 return (False,)
-            
+
         try:
             conSuc, sock = connectETController(self.ip_address)
         except:
@@ -71,7 +72,7 @@ class Robot:
             return [True, f"Robot connected at IP:{self.ip_address}"]
         else:
             return [False, f"Cannot connect at IP:{self.ip_address}"]
-        
+
     def disconnect(self):
         '''
         This function is used to disconnect from the robot.
@@ -87,7 +88,7 @@ class Robot:
             self.sock = None
             return [True, "Robot Disconnect Success, Robot was already disconected."]
 
-    def execute(self, command, params):
+    def execute(self, command, params=None):
         '''
         This function is used to execute commands on the robot. Better version of the official sendCMD function.
 
@@ -99,7 +100,10 @@ class Robot:
             list: [int, str] -> [ID, "Result of the command"]
         '''
         if self.connect_success:
-            suc, result, id = self.sendCMD(self.sock, command, params)
+            if (not params):
+                suc, result, id = self.sendCMD(command)
+            else:
+                suc, result, id = self.sendCMD(command, params)
 
             if suc:
                 return [id, result]
@@ -121,14 +125,15 @@ class Robot:
         '''
 
         if self.connect_success:
-            suc, result, id = self.sendCMD(self.sock, f"get{variable_type}", {"addr": variable_address})
+            suc, result, id = self.sendCMD(self.sock, f"get{variable_type}", {
+                                           "addr": variable_address})
             if suc:
                 return [id, result]
             else:
                 raise Exception(f"Can not get variable: {result}")
         else:
             return [False, f"Robot not connected at IP:{self.ip_address}"]
-        
+
     def setVariable(self, variable_type, variable_address, variable_value):
         '''
         This function is used to set the value of a variable from the robot.
@@ -142,7 +147,8 @@ class Robot:
             list: [int, str] -> [ID, Result of the command]
         '''
         if self.connect_success:
-            suc, result, id = self.sendCMD(self.sock, f"set{variable_type}", {"addr": variable_address, "value": variable_value})
+            suc, result, id = self.sendCMD(f"set{variable_type}", {
+                "addr": variable_address, "value": variable_value})
             if suc:
                 return [id, result]
             else:
@@ -156,34 +162,37 @@ class Robot:
 
         Attributes:
             status (int): The status of the servo, 0 for OFF and 1 for ON.
-        
+
         Returns:
             list: [int, str] -> [ID, Result of the command]
         '''
         if self.connect_success:
-            suc, result, id = self.sendCMD(self.sock, "set_servo_status", {"status": status})
+            suc, result, id = self.sendCMD(
+                self.sock, "set_servo_status", {"status": status})
             if suc:
                 return [id, result]
             else:
                 raise Exception(f"Can not set servo status: {result}")
         else:
             return [False, f"Robot not connected at IP:{self.ip_address}"]
-        
+
     def runJbi(self, jbi_filename):
         '''
         This function is used to run a jbi file on the robot.
 
         Attributes:
             jbi_filename (string): The name of the jbi file we want to run.
-        
+
         Returns:
             list: [int, str] -> [ID, Result of the command]
         '''
 
         if self.connect_success:
-            suc, result, id = self.sendCMD(self.sock, "checkJbiExist", {"filename": jbi_filename})
+            suc, result, id = self.sendCMD(self.sock, "checkJbiExist", {
+                                           "filename": jbi_filename})
             if suc and result == 1:
-                suc, result, id = self.sendCMD(self.sock, "runJbi", {"filename": jbi_filename})
+                suc, result, id = self.sendCMD(
+                    self.sock, "runJbi", {"filename": jbi_filename})
                 if suc:
                     return [id, result]
                 else:
@@ -192,7 +201,7 @@ class Robot:
                 raise Exception(f"Can not run jbi: {result}")
         else:
             return [False, f"Robot not connected at IP:{self.ip_address}"]
-    
+
     def getJbiState(self):
         '''
         This function is used to get the state of the jbi file running on the robot.
@@ -208,15 +217,12 @@ class Robot:
                 raise Exception(f"Can not get jbi state: {result}")
         else:
             return [False, f"Robot not connected at IP:{self.ip_address}"]
-    
+
     def __repr__(self):
         return f'Robot Object at IP:{self.ip_address}, Connection:{self.connect_success}'
 
 
-
-
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     # robot_ip = "192.168.0.2"
     # i = 5
     # conSuc,sock=connectETController(robot_ip)
@@ -271,9 +277,7 @@ if __name__ == "__main__":
     #                 suc, result , id=sendCMD(sock,"getJbiState")
     #                 checkRunning=result[" runState "]
     #                 time. sleep (0.1)
-        
-    
-     
+
     # if(conSuc):
     #     suc, result , id=sendCMD(sock,"checkJbiExist",{"filename": jbi_filename })
     #     if (suc and result ==1):
@@ -292,7 +296,8 @@ if __name__ == "__main__":
     #     print("Cannot Connect to Robot")
     #     print("Exiting...")
     #     sys.exit(100)
-    
+
+
 def disconnectETController(sock):
     if (sock):
         sock.close()
